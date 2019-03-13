12a0b2c3a647cfd2744a20e5b0aead97ffdeb898
Peyman Akhlaghi <codecore@Peymans-MBP.gateway>
2019-03-14 01:42:18 +1100

    build is added

---
+++ build/precache-manifest.afad7c44653629150286e9b9556c1ab5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f8c9a9deedb3a29839bc",
+    "url": "/static/js/main.0669d51c.chunk.js"
+  },
+  {
+    "revision": "9213c98268ec5f1e960c",
+    "url": "/static/js/2.093fd198.chunk.js"
+  },
+  {
+    "revision": "f8c9a9deedb3a29839bc",
+    "url": "/static/css/main.92b1d476.chunk.css"
+  },
+  {
+    "revision": "01e710b46524a6f87d365730391242af",
+    "url": "/index.html"
+  }
+];
